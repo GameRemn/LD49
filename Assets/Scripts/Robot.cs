13bd11ebf8ec2b726a9cf3ef86d6f6da4a9fc6d8
Remn <gameremn@mail.ru>
2021-10-02 17:24:19 +0300

    Add DOTween

--- Assets/Scripts/Robot.cs
+++ Assets/Scripts/Robot.cs
@@ -3,11 +3,20 @@ using System.Collections;
 using System.Collections.Generic;
 using ScriptableObjectArchitecture;
 using UnityEngine;
+using UnityEngine.Events;
 
 public abstract class Robot : MonoBehaviour
 {
     public GameEventBase onEnableEvent;
     public GameEventBase onDisableEvent;
+    public UnityEvent<Fraction> onFractionChange;
+    public Fraction Fraction
+    {
+        get => _fraction;
+        set => _fraction = value;
+    }
+
+    private Fraction _fraction;
 
     public void OnEnable()
     {
@@ -23,4 +32,11 @@ public abstract class Robot : MonoBehaviour
     {
         Destroy(gameObject);
     }
+}
+
+public enum Fraction
+{
+    Red,
+    Blue,
+    Neutral
 }
