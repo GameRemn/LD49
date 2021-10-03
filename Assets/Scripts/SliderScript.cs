f1ebe5c602d52a6c6c22949174024266069b57c3
KristinaPrro <84905463+KristinaPrro@users.noreply.github.com>
2021-10-03 21:02:41 +0300

    EffectSystDelee

---
+++ Assets/Scripts/SliderScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SliderScript : MonoBehaviour
+{
+    //Muisic;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/UIScript.cs
+++ Assets/Scripts/UIScript.cs
@@ -4,13 +4,10 @@ using UnityEngine;
 
 public class UIScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void P()
     {
         
     }
-
-    // Update is called once per frame
     void Update()
     {
         
